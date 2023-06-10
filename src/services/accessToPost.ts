07dbdada33fbc7bcfda8601484072bd5687c10f4
MON <mrmon93+git@gmail.com>
2023-06-11 04:39:54 +0900

    fix: revive method

--- src/services/accessToPost.ts
+++ src/services/accessToPost.ts
@@ -5,3 +5,11 @@ export const getPost = async (slug: string) => {
   const postData = await getPostDataBySlug(slug);
   return postData;
 };
+
+/** 記事のスラッグ一覧を取得 */
+export const getAllPostSlugs = () => {
+  // 全記事を取得、スラッグのみの配列に変換
+  const metaList = getSortedPostsMeta();
+  const slugs = metaList.map((meta) => meta.slug);
+  return slugs;
+};
