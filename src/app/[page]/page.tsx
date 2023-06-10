f8436bdbe86c97511d5625f1e18a7398d9bec37c
MON <mrmon93+git@gmail.com>
2023-06-11 03:57:11 +0900

    feat: add page

---
+++ src/app/[page]/page.tsx
@@ -0,0 +1,28 @@
+import { getPosts } from "services/accessToPage";
+import PostCard from "components/card";
+import Pagination from "components/pagination";
+
+export default function Page({ params }: { params: { page: number } }) {
+  const posts = getPosts(params.page);
+  return (
+    <div className="container mx-auto max-w-5xl mb-14">
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {posts.posts.map((post) => {
+          return (
+            <>
+              {/* @ts-expect-error Server Component */}
+              <PostCard key={post.slug} data={post} size="small" />
+            </>
+          );
+        })}
+      </div>
+
+      <Pagination
+        type="all"
+        currentPage={params.page}
+        totalPage={posts.totalPage}
+        text={""}
+      />
+    </div>
+  );
+}
