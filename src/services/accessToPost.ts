9e02965cbc87375de34d05f1120bd3860742fb5e
MON <mrmon93+git@gmail.com>
2023-06-11 04:02:42 +0900

    refactor: del without using method

--- src/services/accessToPost.ts
+++ src/services/accessToPost.ts
@@ -5,11 +5,3 @@ export const getPost = async (slug: string) => {
   const postData = await getPostDataBySlug(slug);
   return postData;
 };
-
-/** 記事のスラッグ一覧を取得 */
-export const getAllPostSlugs = () => {
-  // 全記事を取得、スラッグのみの配列に変換
-  const metaList = getSortedPostsMeta();
-  const slugs = metaList.map((meta) => meta.slug);
-  return slugs;
-};
