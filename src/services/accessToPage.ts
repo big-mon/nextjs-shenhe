ad880288fcfc43c8923680c0ae69feaf4b58b13f
MON <mrmon93+git@gmail.com>
2023-06-12 01:50:53 +0900

    fix: page count

--- src/services/accessToPage.ts
+++ src/services/accessToPage.ts
@@ -4,10 +4,11 @@ import { getSortedPostsMeta } from "repositories/local/post";
 /** トップページ(ページ指定あり)向けの記事一覧と全ページ数を取得 */
 export const getPosts = (page: number) => {
   const allPosts = getSortedPostsMeta();
-  const posts = allPosts.slice((page - 1) * PER_PAGE - 1, page * PER_PAGE);
+  const posts = allPosts.slice((page - 1) * PER_PAGE - 1, page * PER_PAGE - 1);
+  const remainPosts = allPosts.length - (PER_PAGE - 1);
 
   return {
     posts: posts,
-    totalPage: Math.ceil(allPosts.length / PER_PAGE),
+    totalPage: Math.ceil(remainPosts / PER_PAGE + 1),
   };
 };
