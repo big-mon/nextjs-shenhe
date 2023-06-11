9dbcd5d04b53878d7c3b8a94658948541a7bed34
MON <mrmon93+git@gmail.com>
2023-06-12 01:50:59 +0900

    chore: sample

--- src/constants/setting.ts
+++ src/constants/setting.ts
@@ -3,7 +3,7 @@ export const SITE_IMAGE: string =
   "https://res.cloudinary.com/damonge/image/upload/f_auto,q_auto,fl_progressive,c_limit,h_630,w_1200/Hero/YS4_5968_69_70-2-3_x9dlrp";
 
 /** list articles per page */
-export const PER_PAGE: number = 12;
+export const PER_PAGE: number = 6;
 
 /** Whether to read the image path to the specified external service */
 export const IS_USE_EXTERNAL_IMAGE: boolean = true;
