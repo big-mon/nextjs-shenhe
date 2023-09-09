18969fe3b2788eb155bbaafc244538eb48e0104e
MON <mrmon93+git@gmail.com>
2023-09-10 04:27:15 +0900

    chore: edit

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -86,6 +86,6 @@ export default async function Page({ params }: { params: { slug: string } }) {
   );
 }
 
-export async function generateStaticParams() {
+export function generateStaticParams() {
   return getAllPostSlugs();
 }
