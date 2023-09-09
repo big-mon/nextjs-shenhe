1efe8214a31c69e58d11e1d9c6e47a3e6700e08e
MON <mrmon93+git@gmail.com>
2023-09-09 19:29:47 +0900

    chore: edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,21 +8,13 @@ export default function Page() {
     <div className="container mx-auto max-w-5xl mb-14">
       <div className="grid lg:grid-cols-2 gap-6">
         {posts.posts.slice(0, 2).map((post) => {
-          return (
-            <>
-              <PostCard key={post.slug} data={post} size="large" />
-            </>
-          );
+          return <PostCard key={post.slug} data={post} size="large" />;
         })}
       </div>
 
       <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-5">
         {posts.posts.slice(2).map((post) => {
-          return (
-            <>
-              <PostCard key={post.slug} data={post} size="small" />
-            </>
-          );
+          return <PostCard key={post.slug} data={post} size="small" />;
         })}
       </div>
 
