2c7faa4dbe1786fc7a917c9a6e8debd132a75c19
MON <mrmon93+github@gmail.com>
2025-02-02 13:10:54 +0900

    refactor: 関数へコメントを追加、ファイルパス構造を変更

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,31 +4,8 @@ import path from "path";
 import matter from "gray-matter";
 import { join } from "path";
 
-const postsDirectory = join(process.cwd(), "_post");
-
-function getPostPaths() {
-  return retrieveFiles(postsDirectory);
-}
-
-export function getPostByPath(path: string) {
-  const fileContents = fs.readFileSync(path, "utf8");
-  const { data, content } = matter(fileContents);
-
-  return { ...data, slug: data.slug, content } as Post;
-}
-
-export function getPostBySlug(slug: string) {
-  const allPosts = getAllPosts();
-  return allPosts.find((post) => post.slug === slug);
-}
-
-export function getAllPosts(): Post[] {
-  const paths = getPostPaths();
-  const posts = paths
-    .map((slug) => getPostByPath(slug))
-    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
-  return posts;
-}
+// ブログ記事のディレクトリパス
+const postsDirectory = join(process.cwd(), "_posts");
 
 /**
  * 指定されたディレクトリ内のファイルパスを再帰的に取得する関数
@@ -48,3 +25,46 @@ const retrieveFiles = (directory: string): string[] => {
     }
   });
 };
+
+/**
+ *  全ブログ記事のファイルパスを取得する関数
+ * @returns ブログ記事のファイルパスの配列
+ */
+function getPostPaths(): string[] {
+  return retrieveFiles(postsDirectory);
+}
+
+/**
+ * ファイルパスからブログ記事を取得する関数
+ * @param path - ブログ記事のファイルパス
+ * @returns ブログ記事
+ */
+export function getPostByPath(path: string): Post {
+  const fileContents = fs.readFileSync(path, "utf8");
+  const { data, content } = matter(fileContents);
+
+  return { ...data, slug: data.slug, content } as Post;
+}
+
+/**
+ * スラッグからブログ記事を取得する関数
+ * @param slug - ブログ記事のスラッグ
+ * @returns ブログ記事
+ */
+export function getPostBySlug(slug: string): Post | undefined {
+  const allPosts = getAllPosts();
+  return allPosts.find((post) => post.slug === slug);
+}
+
+/**
+ * 全ブログ記事を取得する関数
+ * @returns ブログ記事の配列
+ */
+export function getAllPosts(): Post[] {
+  const paths = getPostPaths();
+  const posts = paths
+    .map((slug) => getPostByPath(slug))
+    // 日付の降順でソート
+    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
+  return posts;
+}
