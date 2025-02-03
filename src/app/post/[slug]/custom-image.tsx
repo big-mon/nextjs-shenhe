36f4fd97b344c30934455770257b6e268193aed9
MON <mrmon93+github@gmail.com>
2025-02-04 00:13:04 +0900

    feat: 画像をリンク化

--- src/app/post/[slug]/custom-image.tsx
+++ src/app/post/[slug]/custom-image.tsx
@@ -2,8 +2,6 @@
 
 import { CldImage } from "next-cloudinary";
 import { getCloudinaryImageUrl } from "@lib/cloudinary";
-import { isExternalLink } from "@lib/url";
-import { CustomLink } from "./custom-link";
 
 type Props = {
   src: string;
@@ -12,21 +10,23 @@ type Props = {
 };
 
 export const CustomImage = ({ src, alt = "", title = "" }: Props) => {
-  const isExternal = isExternalLink(src);
+  const imageUrl = getCloudinaryImageUrl(src, "post");
   const imageOgpUrl = getCloudinaryImageUrl(src, "ogp");
 
   return (
     <span className="relative block">
-      <CldImage
-        src={src}
-        alt={alt}
-        width={860}
-        height={860}
-        placeholder="blur"
-        blurDataURL={imageOgpUrl}
-        namedTransformations={["post"]}
-        sizes="(max-width: 672px) 100vw, 672px"
-      />
+      <a href={imageUrl} target="_blank" rel="noopener noreferrer">
+        <CldImage
+          src={src}
+          alt={alt}
+          width={860}
+          height={860}
+          placeholder="blur"
+          blurDataURL={imageOgpUrl}
+          namedTransformations={["post"]}
+          sizes="(max-width: 672px) 100vw, 672px"
+        />
+      </a>
       {title === "" ? null : (
         <span className="block text-center text-gray-500">{title}</span>
       )}
