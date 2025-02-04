44e8117b4b9ee039d5f353b8147e167ae86994dc
MON <mrmon93+github@gmail.com>
2025-02-04 22:46:53 +0900

    feat: カテゴリーページに対応

---
+++ src/app/category/[category]/[page]/page.tsx
@@ -0,0 +1,35 @@
+import { MoreStories } from "@components/more-stories";
+import Pagination from "@components/pagination";
+import { getAllPosts } from "@lib/blogService";
+import { howTotalPages } from "@lib/pagination";
+import { PER_PAGE } from "@lib/constants";
+
+type Params = {
+  params: Promise<{
+    category: string;
+    page: number;
+  }>;
+};
+
+export default async function Page(props: Params) {
+  const category = decodeURIComponent((await props.params).category);
+  const page = (await props.params).page;
+  const allPosts = getAllPosts().filter((post) => post.category === category);
+  const pagePosts = allPosts.slice((page - 1) * PER_PAGE, page * PER_PAGE);
+  const totalPage = howTotalPages(allPosts);
+
+  return (
+    <main className="container mx-auto max-w-5xl">
+      <MoreStories posts={pagePosts} />
+
+      <div className="mb-16">
+        <Pagination
+          type={"category"}
+          currentPage={page}
+          totalPage={totalPage}
+          prefix={category}
+        />
+      </div>
+    </main>
+  );
+}
