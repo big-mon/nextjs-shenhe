f2c2c2210d35e11e1db9348d7070461bd5ddfdf6
MON <mrmon93+github@gmail.com>
2025-02-15 01:51:53 +0900

    fix: タグの検索方法を修正

--- src/app/tag/[tag]/[page]/page.tsx
+++ src/app/tag/[tag]/[page]/page.tsx
@@ -14,7 +14,9 @@ type Params = {
 export default async function Page(props: Params) {
   const tag = decodeURIComponent((await props.params).tag);
   const page = (await props.params).page;
-  const allPosts = getAllPosts().filter((post) => post.tags.includes(tag));
+  const allPosts = getAllPosts().filter((post) =>
+    post.tags.map((t) => t.toLowerCase()).includes(tag.toLowerCase())
+  );
   const pagePosts = allPosts.slice((page - 1) * PER_PAGE, page * PER_PAGE);
   const totalPage = howTotalPages(allPosts);
 
